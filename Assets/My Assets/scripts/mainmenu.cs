d2eb6120de9584f533e520d8f0fb57b398ad8f4f
CrazyHairGuy <crazyhairguy11@gmail.com>
2017-03-08 09:09:44 -0600

    barebones main manu

---
+++ Assets/My Assets/scripts/mainmenu.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityStandardAssets.CrossPlatformInput;
+using UnityEngine.SceneManagement;
+
+public class mainmenu : MonoBehaviour {
+
+	bool isHovering;
+
+	// Use this for initialization
+
+	void OnMouseEnter(){
+		isHovering = true;
+	}
+
+	void OnMouseExit(){
+		isHovering = false;
+	}
+
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+
+		if (CrossPlatformInputManager.GetButtonUp ("Throw")) {
+			SceneManager.LoadScene ("rev 2 experimental");
+		}
+		
+	}
+}
